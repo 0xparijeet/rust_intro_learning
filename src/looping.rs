66890e0b3e5b64572c64bab0159ccebfff83d962
Parijeet Kapratwar <parijeetkapratwar@Parijeets-MacBook-Air.local>
2024-09-02 06:53:38 +0530

    added README.md

---
+++ src/looping.rs
@@ -0,0 +1,23 @@
+
+// Looping 
+
+/* 
+    loop
+    while
+    for
+*/
+
+pub fn loop_on_five_digit() {
+    let mut x  = 0;
+    loop {
+        println!("loop 😜 {}", x);
+        x += 1;
+        if x == 5 {
+            break;
+        }
+    }
+}
+
+pub fn while_on_name_digit() {
+    
+}
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+
+mod looping;
+
+fn main() {
+    println!("Hello, world!");
+
+    looping::loop_on_five_digit();
+
+}
